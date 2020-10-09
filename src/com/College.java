962ae6d5482156a9089a6c772fb80da438e4277c
Aakriti <aakritiagarwal2@gmail.com>
2020-10-09 23:06:31 +0530

    Update College.java

--- src/com/College.java
+++ src/com/College.java
@@ -6,8 +6,8 @@ public class College {
 	String[] subject;  
 	String publication;
 	ArrayList<Teacher> TeacherList=new ArrayList<Teacher>();
-	public ArrayList<Teacher> findOddStudentForMarks(ArrayList<Teacher> t){
-		for(Teacher t1:t)
+	public ArrayList<Teacher> findOddStudentForMarks(ArrayList<Teacher> teach){
+		for(Teacher t1:teach)
 			TeacherList.add(t1);
 	return TeacherList;
 	}
